2fe98441f30bd0ca135c44da15012f7bd1e775f2
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-04-02 10:45:25 +0700

    menambahkan perulangan untuk mengisi array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -12,5 +12,20 @@ void input()
     {
         cout << "Masukkan Panjang element array : ";
         cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+            
     }
+    cout << "\n---------------------------" << endl;
+    cout << "\nIinputkan Isi element array" << endl;
+    cout << "\n---------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
 }
